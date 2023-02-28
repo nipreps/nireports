8c1e7ca686c8d3b57108929eace2ed2b8b086308
Oscar Esteban <code@oscaresteban.es>
2023-02-28 22:33:57 +0100

    maint: add visualization tests

--- nireports/tests/test_viz.py
+++ nireports/tests/test_viz.py
@@ -1,7 +1,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 #
-# Copyright 2021 The NiPreps Developers <nipreps@gmail.com>
+# Copyright 2023 The NiPreps Developers <nipreps@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
 #
 #     https://www.nipreps.org/community/licensing/
 #
-"""Test viz module"""
+"""Test reportlets module."""
 import os
 from pathlib import Path
 
@@ -30,9 +30,8 @@ import pandas as pd
 import pytest
 
 from .conftest import datadir
-from .generate_data import _create_dtseries_cifti
-from .. import viz
-from niworkflows.viz.plots import fMRIPlot
+from .generate_data import _create_dtseries_cifti 
+from nireports.reportlets.plots import fMRIPlot, plot_carpet
 from niworkflows.utils.timeseries import _cifti_timeseries, _nifti_timeseries
 from niworkflows.interfaces.plotting import _get_tr
 
