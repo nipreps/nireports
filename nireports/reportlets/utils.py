223c5edb431282ced5fb2e392ea06756cd8e3dc0
Ariel Rokem <arokem@gmail.com>
2023-03-21 13:43:21 -0700

    Update nireports/reportlets/utils.py
    Co-authored-by: Oscar Esteban <code@oscaresteban.es>

--- nireports/reportlets/utils.py
+++ nireports/reportlets/utils.py
@@ -322,7 +322,6 @@ def compose_view(bg_svgs, fg_svgs, ref=0, out_file="report.svg"):
         Full paths to input svgs for background.
     fg_svgs : list
         Full paths to input svgs for foreground.
-
     ref : int, optional
         Which panel to use as reference for sizing all panels. Default: 0
 
