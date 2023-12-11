8f941bc3c417222d32954385b181abfb708f131c
celprov <77437752+celprov@users.noreply.github.com>
2023-12-11 12:02:19 -0800

    Apply suggestions from code review
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>

--- nireports/reportlets/xca.py
+++ nireports/reportlets/xca.py
@@ -358,9 +358,9 @@ def compcor_variance_plot(
 
         ax[m].set_yticks([])
         ax[m].set_yticklabels([])
-        for tick in ax[m].xaxis.get_major_ticks():
-            tick.label1.set_fontsize("x-small")
-            tick.label1.set_rotation("vertical")
+        for label in ax[m].xaxis.get_majorticklabels():
+            label.set_fontsize("x-small")
+            label.set_rotation("vertical")
         for side in ["top", "right", "left"]:
             ax[m].spines[side].set_color("none")
             ax[m].spines[side].set_visible(False)
