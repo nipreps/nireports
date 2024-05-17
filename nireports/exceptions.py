36c0319df28992a4f8603b9cedf3f66b3dbc4e7c
Ross Blair <rosswilsonblair@gmail.com>
2024-05-17 13:18:10 -0500

    run ruff format

--- nireports/exceptions.py
+++ nireports/exceptions.py
@@ -1,9 +1,13 @@
 class NiReportsException(Exception):
     pass
 
+
 class ReportletException(NiReportsException):
     pass
 
+
 class RequiredReportletException(ReportletException):
     def __init__(self, config):
-        self.args = (f'No content found while generated reportlet listed as required with the following config: {config}',)
+        self.args = (
+            f"No content found while generated reportlet listed as required with the following config: {config}",
+        )
