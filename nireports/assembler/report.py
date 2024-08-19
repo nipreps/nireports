c980e130d8e5c7b1470b31e9a97870a94bf22227
Chris Markiewicz <effigies@gmail.com>
2024-08-19 11:42:40 -0400

    test: Fix expected length of widget (see 4d43a88)

--- nireports/assembler/report.py
+++ nireports/assembler/report.py
@@ -96,7 +96,7 @@ class Report:
        ...     dirs_exist_ok=True,
        ... )
        >>> REPORT_BASELINE_LENGTH = 41770
-       >>> RATING_WIDGET_LENGTH = 83308
+       >>> RATING_WIDGET_LENGTH = 83343
 
 
     Examples
