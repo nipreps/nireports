55e470016c732d9cd45885f4f5bd937135a7cc65
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-03-03 17:59:07 +0100

    STY: Apply ruff/refurb preview rule FURB140
    FURB140 Use `itertools.starmap` instead of the generator

--- nireports/tests/test_reportlets.py
+++ nireports/tests/test_reportlets.py
@@ -25,7 +25,7 @@
 import contextlib
 import os
 import warnings
-from itertools import permutations
+from itertools import permutations, starmap
 from pathlib import Path
 
 import matplotlib as mpl
@@ -596,10 +596,9 @@ def test_create_cmap(outdir):
         key1 == key2
         for key1, key2 in zip(orig_cmap._segmentdata.keys(), ls_cmap._segmentdata.keys())
     ]
-    assert [
-        np.allclose(val1, val2)
-        for val1, val2 in zip(orig_cmap._segmentdata.values(), ls_cmap._segmentdata.values())
-    ]
+    assert list(
+        starmap(np.allclose, zip(orig_cmap._segmentdata.values(), ls_cmap._segmentdata.values()))
+    )
 
     fig, ax = plt.subplots(1, 1, figsize=(12, 10), constrained_layout=True)
     _ = plt.colorbar(plt.cm.ScalarMappable(cmap=ls_cmap), cax=ax, orientation="horizontal")
